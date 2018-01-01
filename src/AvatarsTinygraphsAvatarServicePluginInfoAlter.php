74bdc92d9bfeeb19e5037e569c65ccea9507dc0a
dpi <pro@danielph.in>
2018-01-01 23:16:18 +0800

    Added dependency to this module.

--- src/AvatarsTinygraphsAvatarServicePluginInfoAlter.php
+++ src/AvatarsTinygraphsAvatarServicePluginInfoAlter.php
@@ -19,11 +19,26 @@ class AvatarsTinygraphsAvatarServicePluginInfoAlter implements EventSubscriberIn
    *   The event.
    */
   public function alterServiceInfo(AvatarKitServiceDefinitionAlterEvent $event) {
-    // Avatar Kit service plugin manager will pick up common services simply by
-    // making these services use a real class instead of an abstract.
     $definitions = $event->getDefinitions();
-    $definitions['avatars_ak_common:tinygraphs_isogrids']['class'] = Tinygraphs::class;
-    $definitions['avatars_ak_common:tinygraphs_square']['class'] = Tinygraphs::class;
+
+    $ids = [
+      'tinygraphs_isogrids',
+      'tinygraphs_square',
+    ];
+
+    foreach ($ids as $id) {
+      $id = 'avatars_ak_common:' . $id;
+
+      // Avatar Kit service plugin manager will pick up common services simply by
+      // making these services use a real class instead of an abstract.
+      $definitions[$id]['class'] = Tinygraphs::class;
+
+      // Need to add a dependency to this module, since once this module is
+      // uninstalled, the common deriver will revert to the abstract class, which
+      // the plugin manager ignores.
+      $definitions[$id]['config_dependencies']['module'][] = 'avatars_tinygraphs';
+    }
+
     $event->setDefinitions($definitions);
   }
 
